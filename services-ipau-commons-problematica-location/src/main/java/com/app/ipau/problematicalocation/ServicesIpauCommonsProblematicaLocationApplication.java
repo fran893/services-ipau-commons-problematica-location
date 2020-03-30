68ec2ad2980b137e7c6696f31f0901457f55582e
Franco <alvarez.fr893@gmail.com>
2020-03-30 14:50:47 -0300

    primer commit

---
+++ services-ipau-commons-problematica-location/src/main/java/com/app/ipau/problematicalocation/ServicesIpauCommonsProblematicaLocationApplication.java
@@ -0,0 +1,13 @@
+package com.app.ipau.problematicalocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicesIpauCommonsProblematicaLocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicesIpauCommonsProblematicaLocationApplication.class, args);
+	}
+
+}
