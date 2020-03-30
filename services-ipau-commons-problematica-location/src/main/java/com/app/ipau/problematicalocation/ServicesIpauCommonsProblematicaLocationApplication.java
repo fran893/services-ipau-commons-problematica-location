6cbc7dc6e3cdacd582f3b4a2f83218aef712b4c2
Franco <alvarez.fr893@gmail.com>
2020-03-30 15:05:48 -0300

    se agrego modelo de problematica location con marca de dudosa

--- services-ipau-commons-problematica-location/src/main/java/com/app/ipau/problematicalocation/ServicesIpauCommonsProblematicaLocationApplication.java
+++ services-ipau-commons-problematica-location/src/main/java/com/app/ipau/problematicalocation/ServicesIpauCommonsProblematicaLocationApplication.java
@@ -1,13 +1,13 @@
 package com.app.ipau.problematicalocation;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+@EntityScan("com.app.ipau.commons.problematicas.models.entities")
 public class ServicesIpauCommonsProblematicaLocationApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ServicesIpauCommonsProblematicaLocationApplication.class, args);
-	}
 
 }
